a79ef2a98c422bfc1153a370078daf74e688f427
adamhinton <adam.d.hinton@gmail.com>
2024-05-21 10:41:09 -0400

    rework app config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
-function App(): React.JSX.Element {
+const App = (): React.JSX.Element => {
   return (
     <SafeAreaView>
       <Text>Hello world</Text>
     </SafeAreaView>
   );
-}
+};
 
 export default App;
