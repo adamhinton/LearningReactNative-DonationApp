037b037956cb8d2918ebd96bfc69bf9c2c98c8ea
adamhinton <adam.d.hinton@gmail.com>
2024-05-22 00:49:26 -0400

    more nav install stuff

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = (): React.JSX.Element => {
   return (
-    <SafeAreaView>
-      <Text>Hello world</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>Hello world</Text>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
