4a950cb69e69da4f6e1e7abc9ca30439ae0a51ba
adamhinton <adam.d.hinton@gmail.com>
2024-05-22 11:26:07 -0400

    globalStyle

--- Navigation/Screens/Home/Home.tsx
+++ Navigation/Screens/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import globalStyle from '../../../assets/styles/GlobalStyle';
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={globalStyle.backgroundWhite}>
       <View>
         <Text>Hello World!!</Text>
       </View>
