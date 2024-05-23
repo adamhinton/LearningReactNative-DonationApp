9ff87f78390d6f8989d306dbdd6b9bd7fdc5b703
adamhinton <adam.d.hinton@gmail.com>
2024-05-23 08:51:56 -0400

    Search refs

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,14 +1,24 @@
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import React from 'react';
+import React, {useRef} from 'react';
 import {Pressable, TextInput} from 'react-native';
 import style from './style';
 
 const Search = () => {
+  const textInputRef = useRef<TextInput>(null);
+
+  const handleFocus = () => {
+    textInputRef.current!.focus();
+  };
+
   return (
     <Pressable style={style.searchInputContainer}>
       <FontAwesomeIcon icon={faSearch} />
-      <TextInput style={style.searchInput} />
+      <TextInput
+        ref={textInputRef}
+        onPress={handleFocus}
+        style={style.searchInput}
+      />
     </Pressable>
   );
 };
