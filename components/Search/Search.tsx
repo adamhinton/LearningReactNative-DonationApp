51ddc889e6036cc315aef61226c2087c60f42316
adamhinton <adam.d.hinton@gmail.com>
2024-05-23 08:47:50 -0400

    Search basic styling

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,7 +1,16 @@
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
+import {Pressable, TextInput} from 'react-native';
+import style from './style';
 
 const Search = () => {
-  return null;
+  return (
+    <Pressable style={style.searchInputContainer}>
+      <FontAwesomeIcon icon={faSearch} />
+      <TextInput style={style.searchInput} />
+    </Pressable>
+  );
 };
 
 export default Search;
--- components/Search/style.ts
+++ components/Search/style.ts
@@ -1,5 +1,19 @@
 import {StyleSheet} from 'react-native';
+import {horizontalScale, verticalScale} from '../../assets/styles/scaling';
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  searchInput: {
+    borderWidth: 1,
+    flex: 1,
+    marginLeft: horizontalScale(6),
+  },
+  searchInputContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: horizontalScale(16),
+    backgroundColor: '#F3F5F9',
+    height: verticalScale(50),
+    alignItems: 'center',
+  },
+});
 
 export default style;
