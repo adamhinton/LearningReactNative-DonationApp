eedf80d9d34c0171b25c37fcdc6984130aa58cb0
adamhinton <adam.d.hinton@gmail.com>
2024-05-23 08:37:11 -0400

    clear unused imports

--- Navigation/Screens/Home/Home.tsx
+++ Navigation/Screens/Home/Home.tsx
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyle from '../../../assets/styles/GlobalStyle';
-import Header from '../../../components/Header/Header';
-import Button from '../../../components/Button/Button';
-import Tab from '../../../components/Tab/Tab';
-import Badge from '../../../components/Badge/Badge';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
   return (
