462efd85f09bfe394c95bc27986c04b7acd8dbbf
adamhinton <adam.d.hinton@gmail.com>
2024-05-23 18:49:18 -0400

    input handling in Search

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,12 +1,24 @@
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import {Pressable, TextInput} from 'react-native';
 import style from './style';
 import {scaleFontSize} from '../../assets/styles/scaling';
 
-const Search = () => {
+type Props = {
+  onSearch: (value: string) => void;
+};
+
+const Search = (props: Props) => {
+  const {onSearch} = props;
+
   const textInputRef = useRef<TextInput>(null);
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (value: string) => {
+    setSearch(value);
+    onSearch(value);
+  };
 
   const handleFocus = () => {
     textInputRef.current!.focus();
@@ -23,6 +35,8 @@ const Search = () => {
         ref={textInputRef}
         onPress={handleFocus}
         style={style.searchInput}
+        value={search}
+        onChangeText={val => handleSearch(val)}
       />
     </Pressable>
   );
