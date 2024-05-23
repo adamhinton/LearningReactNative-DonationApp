1629c0853b918e07211f14023ed6b623d6a1b6df
adamhinton <adam.d.hinton@gmail.com>
2024-05-23 08:39:30 -0400

    dummy Search

--- Navigation/Screens/Home/Home.tsx
+++ Navigation/Screens/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyle from '../../../assets/styles/GlobalStyle';
+import Search from '../../../components/Search/Search';
 
 const Home = () => {
   return (
-    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]} />
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <Search />
+    </SafeAreaView>
   );
 };
 
