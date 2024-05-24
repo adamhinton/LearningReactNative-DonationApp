a93dc397e83605e73e876add6af0eec585a27c93
adamhinton <adam.d.hinton@gmail.com>
2024-05-24 12:21:12 -0400

    User reducer

---
+++ redux/reducers/User.ts
@@ -0,0 +1,33 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+type State = {
+  firstName: string;
+  lastName: string;
+  userId: number;
+};
+
+const initialState: State = {
+  firstName: 'Nata',
+  lastName: 'Vacheishvili',
+  userId: 1,
+};
+
+export const User = createSlice({
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    updateFirstName: (state, action: PayloadAction<string>) => {
+      state.firstName = action.payload;
+    },
+    updateLastName: (state, action: PayloadAction<string>) => {
+      state.lastName = action.payload;
+    },
+    updateUserId: (state, action: PayloadAction<number>) => {
+      state.userId = action.payload;
+    },
+  },
+});
+
+export const {updateFirstName, updateLastName, updateUserId} = User.actions;
+
+export default User.reducer;
