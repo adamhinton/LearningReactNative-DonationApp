582f09c82f57bb233e0ccf81f2137661cb180a06
adamhinton <adam.d.hinton@gmail.com>
2024-05-24 15:42:03 -0400

    installing redux-logger

--- Navigation/Screens/Home/Home.tsx
+++ Navigation/Screens/Home/Home.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyle from '../../../assets/styles/GlobalStyle';
 import Header from '../../../components/Header/Header';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../../../redux/store';
+import {Pressable, Text} from 'react-native';
+import {updateFirstName} from '../../../redux/reducers/User';
 
 const Home = () => {
   const user = useSelector((state: RootState) => state.user);
+
+  const dispatch = useDispatch();
 
   const {firstName, lastName} = user;
 
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={`${firstName} ${lastName}`} type={1} />
+      <Pressable onPress={() => dispatch(updateFirstName('N'))}>
+        <Text>Change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
