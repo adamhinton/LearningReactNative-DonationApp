e3fee3ad61c308e31ca73b4f28cfab7602ed4ff3
adamhinton <adam.d.hinton@gmail.com>
2024-05-24 12:35:17 -0400

    typesafe store

--- Navigation/Screens/Home/Home.tsx
+++ Navigation/Screens/Home/Home.tsx
@@ -1,37 +1,16 @@
-/* eslint-disable react-native/no-inline-styles */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import globalStyle from '../../../assets/styles/GlobalStyle';
-import Search from '../../../components/Search/Search';
-import SingleDonationItem from '../../../components/SingleDonationItem/SingleDonationitem';
-import {View} from 'react-native';
-import {horizontalScale} from '../../../assets/styles/scaling';
+import Header from '../../../components/Header/Header';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../../redux/store';
 
 const Home = () => {
+  const user = useSelector((state: RootState) => state.user.firstName);
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      {/* <Search onSearch={val => console.log('val:', val)} /> */}
-      {/* Two SDIs */}
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: horizontalScale(24),
-        }}>
-        <SingleDonationItem
-          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
-          badgeTitle="Environment"
-          donationTitle="Tree Cactus"
-          price={44}
-        />
-        <SingleDonationItem
-          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
-          badgeTitle="Environment"
-          donationTitle="Tree Cactus"
-          price={44}
-        />
-      </View>
+      <Header title={'Nata'} type={1} />
     </SafeAreaView>
   );
 };
