122745820e5f50270e21b01ffb8a0cc47012f90c
adamhinton <adam.d.hinton@gmail.com>
2024-05-25 11:35:44 -0400

    persistedReducer

--- redux/store.ts
+++ redux/store.ts
@@ -3,13 +3,23 @@ import {combineReducers} from 'redux';
 import User from './reducers/User';
 import {configureStore} from '@reduxjs/toolkit';
 import {logger} from 'redux-logger';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const rootReducer = combineReducers({
   user: User,
 });
 
+const configuration = {
+  key: 'root',
+  storage: AsyncStorage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(configuration, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   // middleware: getDefaultMiddleware => {
   //   return getDefaultMiddleware().concat(logger);
   // },
