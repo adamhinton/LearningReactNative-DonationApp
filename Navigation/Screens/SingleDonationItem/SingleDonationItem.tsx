c6fd1a4248eb4a425d9099e9cbcff8a1e12334a9
adamhinton <adam.d.hinton@gmail.com>
2024-06-02 18:07:40 -0400

    About to start Go Back nav

--- Navigation/Screens/SingleDonationItem/SingleDonationItem.tsx
+++ Navigation/Screens/SingleDonationItem/SingleDonationItem.tsx
@@ -1,6 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../../redux/store';
 
-const SingleDonationItem = () => {};
+const SingleDonationItem = () => {
+  const donationItemInformation = useSelector(
+    (state: RootState) => state.donations.selectedDonationInformation,
+  );
+  console.log('donationItemInformation:', donationItemInformation);
+  return null;
+};
 
 export default SingleDonationItem;
