8340d2364bf4536bc0d3c8988dfe6a2ba6416e3a
adamhinton <adam.d.hinton@gmail.com>
2024-06-03 09:41:42 -0400

    MNav login

--- Navigation/MainNavigation.tsx
+++ Navigation/MainNavigation.tsx
@@ -3,12 +3,14 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Screens/Home/Home';
 import {Routes} from './Routes';
 import SingleDonationItem from './Screens/SingleDonationItem/SingleDonationItem';
+import Login from './Screens/Login/Login';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
   return (
     <Stack.Navigator screenOptions={{header: () => null, headerShown: false}}>
+      <Stack.Screen name={Routes.Login} component={Login} />
       <Stack.Screen name={Routes.Home} component={Home} />
       <Stack.Screen
         name={Routes.SingleDonationItem}
