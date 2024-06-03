383872db25a3ff046015b8491c64f5e09dec88bb
adamhinton <adam.d.hinton@gmail.com>
2024-06-03 09:45:06 -0400

    Input skeleton component

--- Navigation/Screens/Login/Login.tsx
+++ Navigation/Screens/Login/Login.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
+import globalStyle from '../../../assets/styles/GlobalStyle';
 
 const Login = () => {
-  return <></>;
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]} />
+  );
 };
 
 export default Login;
