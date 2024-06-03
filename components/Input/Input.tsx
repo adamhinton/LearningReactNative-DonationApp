5323e78c5ab9c9e8435034b5825a1997d62df844
adamhinton <adam.d.hinton@gmail.com>
2024-06-03 09:48:02 -0400

    basics of Input.tsx

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import globalStyle from '../../assets/styles/GlobalStyle';
 
 const Input = () => {
-  return <></>;
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Input />
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Input;
