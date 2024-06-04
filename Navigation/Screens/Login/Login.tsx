6bedfc8bbee60277d220a24065e20bdff03bb1f4
adamhinton <adam.d.hinton@gmail.com>
2024-06-04 10:14:19 -0400

    welcome back header in Login

--- Navigation/Screens/Login/Login.tsx
+++ Navigation/Screens/Login/Login.tsx
@@ -3,6 +3,7 @@ import {SafeAreaView, ScrollView} from 'react-native';
 import globalStyle from '../../../assets/styles/GlobalStyle';
 import Input from '../../../components/Input/Input';
 import style from './style';
+import Header from '../../../components/Header/Header';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -14,6 +15,7 @@ const Login = () => {
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={style.container}>
+        <Header type={1} title="Welcome Back" />
         <Input
           label={'Email'}
           placeholder="Enter your email"
