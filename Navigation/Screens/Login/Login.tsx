8ded5deff7a697f75be8664949137180a02ac4f2
adamhinton <adam.d.hinton@gmail.com>
2024-06-04 10:01:18 -0400

    email state in Login

--- Navigation/Screens/Login/Login.tsx
+++ Navigation/Screens/Login/Login.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 import globalStyle from '../../../assets/styles/GlobalStyle';
 import Input from '../../../components/Input/Input';
 import style from './style';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <ScrollView
